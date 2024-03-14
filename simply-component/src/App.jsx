e3410e5cc4b583be0a52345a97f55ccd185e5d9f
Dogan Yildirim <dgnyldrmcoc@gmail.com>
2024-03-14 04:24:21 +0300

    React oluşturuldu.

---
+++ simply-component/src/App.jsx
@@ -0,0 +1,17 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        Selam
+      </div>
+    </>
+  )
+}
+
+export default App
