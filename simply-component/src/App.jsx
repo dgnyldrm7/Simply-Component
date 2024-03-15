b7c016daa01246d98bb1d78bf51871791198125a
Dogan Yildirim <dgnyldrmcoc@gmail.com>
2024-03-15 03:18:12 +0300

    Görseller eklendi ve komponentler yazıldı.

--- simply-component/src/App.jsx
+++ simply-component/src/App.jsx
@@ -1,13 +1,53 @@
+//bootstrap import
+import 'bootstrap/dist/css/bootstrap.css'
 import './App.css'
 import Card from './Components/Card'
+import Angular from './images/angular.jpg'
+import Bootstrap from './images/bootstrap5.png'
+import Csharp from './images/ccsharp.png'
+import KompleWeb from './images/kompleweb.jpg'
+
 
 function App() {
 
 
   return (
     <>
-      <Card title="Kurs1" desc="Açıklama1" />
-      <Card title="Kurs1" desc="Açıklama1" />
+    
+    <div className="bg-success navbar navbar-lg">
+      <div className="container">
+      <h3>Card Component</h3>
+      </div>
+    </div>
+
+    <div className="container">
+      <div className="row">
+        <div className="col-md-3">
+          <div className="d-flex justify-content-center">
+            <Card title="Angular" desc="Powerful, extensible, and feature-packed frontend toolkit. Build and customize with Sass, utilize prebuilt grid system and components, and bring projects to life with powerful JavaScript plugins." image={Angular}/>
+          </div>
+        </div>
+        <div className="col-md-3">
+          <div className="d-flex justify-content-center">
+          <Card title="Bootstrap" desc="Powerful, extensible, and feature-packed frontend toolkit. Build and customize with Sass, utilize prebuilt grid system and components, and bring projects to life with powerful JavaScript plugins." image={Bootstrap}/>
+          </div>
+        </div>
+        <div className="col-md-3">
+          <div className="d-flex justify-content-center">
+          <Card title="Csharp" desc="Powerful, extensible, and feature-packed frontend toolkit. Build and customize with Sass, utilize prebuilt grid system and components, and bring projects to life with powerful JavaScript plugins." image={Csharp}/>
+          </div>
+        </div>
+        <div className="col-md-3">
+          <div className="d-flex justify-content-center">
+          <Card title="Full Web" desc="Powerful, extensible, and feature-packed frontend toolkit. Build and customize with Sass, utilize prebuilt grid system and components, and bring projects to life with powerful JavaScript plugins." image={KompleWeb}/>
+          </div>
+        </div>
+
+        
+
+      </div>
+    </div>      
+
     </>
   )
 }
--- simply-component/src/Components/Card.jsx
+++ simply-component/src/Components/Card.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
 
 const Card = (props) => {
     const title = props.title;
     const desc = props.desc;
+    const image = props.image;
+
   return (
     <div>
-        <h1>title = {title}</h1>
-        <h5>desc = {desc}</h5>
+      <div className="card my-5">
+        <img className='img-fluid' style={{
+          borderRadius:"5px 5px 0 0",
+        }} src={image} alt="image" />
+        <div className="card-body">
+        <h2 className='text-center'>{title}</h2>
+        <h6>{desc}</h6>
+        </div>
+      </div>
+        
+        
     </div>
   )
 }
